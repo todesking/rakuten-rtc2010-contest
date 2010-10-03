7978a4c9f0447f50204f9f95ada7b885431f1a70
todesking <discommunicative@gmail.com>
2010-10-03 21:06:12 +0900

    とりあえずプレイヤーファイルつくった

---
+++ src/Ningengasinu.java
@@ -0,0 +1,57 @@
+import jp.ac.washi.quinte.api.CursorAction;
+import jp.ac.washi.quinte.api.GameInfo;
+import jp.ac.washi.quinte.api.Player;
+import jp.ac.washi.quinte.api.Point;
+import jp.ac.washi.quinte.api.RotateType;
+import jp.ac.washi.quinte.api.SoldierAction;
+import jp.ac.washi.quinte.api.TileInfo;
+
+public class Ningengasinu extends Player {
+	public Ningengasinu() {
+	}
+
+	@Override
+	public CursorAction nextCursorAction(GameInfo info) {
+		for (int x = 0; x < info.getMap().getSize(); x++) {
+			for (int y = 0; y < info.getMap().getSize(); y++) {
+				final TileInfo tile = info.getMap().getTile(Point.create(x, y));
+				final char c;
+				switch (tile.getType()) {
+				case BIG_GATE:
+					c = 'G';
+					break;
+				case SMALL_GATE:
+					c = 'g';
+					break;
+				case ROAD:
+					if (tile.getOwner() == info.getLeftCountry())
+						c = '1';
+					else if (tile.getOwner() == info.getOppositeCountry())
+						c = '2';
+					else if (tile.getOwner() == info.getRightCountry())
+						c = '3';
+					else if (tile.getOwner() == info.getMyCountry())
+						c = '0';
+					else if (tile.getOwner() == null)
+						c = '?';
+					else
+						throw new AssertionError();
+					break;
+				case WALL:
+					c = '#';
+					break;
+				default:
+					throw new AssertionError();
+				}
+				System.out.print(c);
+			}
+			System.out.println();
+		}
+		return new CursorAction(RotateType.CLOCKWISE, 3, 12);
+	}
+
+	@Override
+	public SoldierAction nextSoldierAction(GameInfo info) {
+		return SoldierAction.NONE;
+	}
+}
--- src/Starter.java
+++ src/Starter.java
@@ -4,19 +4,20 @@ import jp.ac.washi.quinte.api.Player;
 
 public class Starter {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        Player p1 = new あなたのチーム名();
-        Player p2 = new RandomPlayer();
-        Player p3 = new HighScorePlayer();
-        Player p4 = new SkeltonPlayer();
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Player p1 = new Ningengasinu();
+		Player p2 = new RandomPlayer();
+		Player p3 = new HighScorePlayer();
+		Player p4 = new SkeltonPlayer();
 
-        GameOption option = new GameOption();
-        option.setEnableSound(true);
-        // setEnableThreadToLimitTime(false) はスレッドを使わないため、デバッグしやすい環境になります
-        option.setEnableThreadToLimitTime(true);
-        GameStarter.start(p1, p2, p3, p4, option);
-    }
+		GameOption option = new GameOption();
+		option.setEnableSound(true);
+		option.setFps(100);
+		// setEnableThreadToLimitTime(false) はスレッドを使わないため、デバッグしやすい環境になります
+		option.setEnableThreadToLimitTime(true);
+		GameStarter.start(p1, p2, p3, p4, option);
+	}
 }

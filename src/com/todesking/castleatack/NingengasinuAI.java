385d5da8a4d93f9b4fec03436c4280569f8353d6
todesking <discommunicative@gmail.com>
2010-10-10 22:11:11 +0900

    アルゴリズムの実装中、ライブラリ追加

--- src/com/todesking/castleatack/NingengasinuAI.java
+++ src/com/todesking/castleatack/NingengasinuAI.java
@@ -1,11 +1,27 @@
 package com.todesking.castleatack;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import jp.ac.washi.quinte.api.CountryInfo;
 import jp.ac.washi.quinte.api.CursorAction;
 import jp.ac.washi.quinte.api.GameInfo;
+import jp.ac.washi.quinte.api.MapInfo;
+import jp.ac.washi.quinte.api.Point;
+import jp.ac.washi.quinte.api.RotateType;
 import jp.ac.washi.quinte.api.SoldierAction;
+import jp.ac.washi.quinte.api.TileInfo;
+import jp.ac.washi.quinte.api.TileType;
+
+import org.apache.commons.lang.NotImplementedException;
+
+import com.google.common.collect.Lists;
 
 public class NingengasinuAI implements PlayerAI {
-	public ActionCommand getNextAction(GameInfo info) {
+	int x = 0;
+
+	public ActionCommand getNextAction(final GameInfo info) {
 		// 行動方針の決定: 攻撃、防御、工作
 		// 攻撃優先
 		// やることがなかったら敵から自分への経路を破壊する
@@ -24,16 +40,38 @@ public class NingengasinuAI implements PlayerAI {
 		// 戦士から対象までの経路を決定する
 
 		// とりあえず辺に沿って移動する
-
 		int[][] targetTilePlacement = getTilePlacement(info);
 
-		// 行動の選択
-		// 経路が連結されていないなら、カーソルで経路をつなぐ
-		// カーソルを使用することでよりよい経路が得られるなら、カーソルを使う
-		// 戦士を経路に沿って移動させる
+		final List<Point> mismatchedPoints =
+			getMismatchedPoints(info, targetTilePlacement);
 
-		// 経路を完成させるためのカーソル操作
-		final CursorAction ca = getCursorAction(info, targetTilePlacement);
+		// 行動の選択 // 経路が連結されていないなら、カーソルで経路をつなぐ
+		// カーソルを使用することでよりよい経路が得られるなら、カーソルを使う
+
+		final CursorAction ca;
+		if (!mismatchedPoints.isEmpty()) {
+			// とりあえず決めうちの経路ができてなかったら作る
+			// 自分とこの兵士に近い道から埋めていく
+			Collections.sort(mismatchedPoints, new Comparator<Point>() {
+				@Override
+				public int compare(Point o1, Point o2) {
+					final Point soldierLocation =
+						info.getMySoldier().getLocation();
+					final int d1 = Util.manhattanDistance(soldierLocation, o1);
+					final int d2 = Util.manhattanDistance(soldierLocation, o2);
+					return Double.compare(d1, d2);
+				}
+			});
+			final Point fillTargetPoint = mismatchedPoints.get(0);
+			ca =
+				getCursorActionForFillRoute(
+					info,
+					targetTilePlacement,
+					fillTargetPoint);
+		} else {
+			// TODO: 経路ができてた場合、嫌がらせなど行う
+			ca = getCursorAction(info, targetTilePlacement);
+		}
 
 		// 経路に沿って移動するための戦士操作
 		final SoldierAction sa = getSoldierAction(info, targetTilePlacement);
@@ -41,16 +79,60 @@ public class NingengasinuAI implements PlayerAI {
 		return new ActionCommand(ca, sa);
 	}
 
+	private CursorAction getCursorActionForFillRoute(GameInfo info,
+			int[][] targetTilePlacement, Point point) {
+		final MapInfo map = info.getMap();
+		final Point nearestMyTile =
+			getNearestTile(
+				info,
+				point,
+				info.getMyCountry(),
+				targetTilePlacement);
+		throw new NotImplementedException();
+	}
+
+	private Point getNearestTile(GameInfo info, Point point,
+			CountryInfo country, int[][] targetTilePlacement) {
+		final MapInfo map = info.getMap();
+		for (Point p : Util.spiralPoints(map.getSize(), point)) {
+			final TileInfo tile = map.getTile(p);
+			if (tile != null
+				&& tile.getType() == TileType.ROAD
+				&& tile.getOwner() == country
+				&& targetTilePlacement[p.y][p.x] == T_DONT_CARE)
+				return p;
+		}
+		return null; // wtf
+	}
+
+	private List<Point> getMismatchedPoints(GameInfo info,
+			int[][] targetTilePlacement) {
+		final List<Point> result = Lists.newArrayList();
+		for (int y = 0; y < targetTilePlacement.length; y++) {
+			for (int x = 0; x < targetTilePlacement[0].length; x++) {
+				switch (targetTilePlacement[y][x]) {
+				case T_DONT_CARE:
+					break; // do nothing
+				case T_MY_ROAD:
+					result.add(Point.create(x, y));
+					break;
+				default:
+					throw new AssertionError();
+				}
+			}
+		}
+		return result;
+	}
+
 	private SoldierAction getSoldierAction(GameInfo info,
 			int[][] targetTilePlacement) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	private CursorAction getCursorAction(GameInfo info,
 			int[][] targetTilePlacement) {
-
-		return null;
+		x = (x + 1) % info.getMap().getSize();
+		return new CursorAction(RotateType.CLOCKWISE, x, 10);
 	}
 
 	final int T_DONT_CARE = 0;
--- src/com/todesking/castleatack/Util.java
+++ src/com/todesking/castleatack/Util.java
@@ -1,12 +1,76 @@
 package com.todesking.castleatack;
 
 import java.io.PrintStream;
+import java.util.Iterator;
 
 import jp.ac.washi.quinte.api.GameInfo;
 import jp.ac.washi.quinte.api.Point;
 import jp.ac.washi.quinte.api.TileInfo;
 
+import com.google.common.collect.UnmodifiableIterator;
+
 public class Util {
+	public static int manhattanDistance(Point p1, Point p2) {
+		return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
+	}
+
+	/**
+	 * centerを中心にしたらせん状の座標を返す。centerは含まず。
+	 * 
+	 * @param map
+	 * @param center
+	 * @return
+	 */
+	public static Iterable<Point> spiralPoints(final int size,
+			final Point center) {
+		return new Iterable<Point>() {
+			@Override
+			public Iterator<Point> iterator() {
+				return new UnmodifiableIterator<Point>() {
+					int i1 = 0;
+					int i2 = 0;
+
+					@Override
+					public boolean hasNext() {
+						return true;
+					}
+
+					@Override
+					public Point next() {
+						// エレガントじゃないなー
+						i2++;
+						if (i1 * 8 <= i2) {
+							i1++;
+							i2 = 0;
+						}
+						if (i2 <= i1) {
+							return center.add(new Point(i1, i2));
+						} else if (i2 <= i1 * 3) {
+							return center.add(new Point(i2 - i1 * 2, i1));
+						} else if (i2 <= i1 * 5) {
+							return center.add(new Point(-i1, i2 - i1 * 4));
+						} else if (i2 <= i1 * 7) {
+							return center.add(new Point(i2 - i1 * 6, -i1));
+						} else {
+							return center.add(new Point(i2 - i1 * 8, i1));
+						}
+					}
+				};
+			}
+		};
+	}
+
+	/**
+	 * min,max is inclusive(like sql)
+	 * 
+	 * @param val
+	 * @param min
+	 * @param max
+	 * @return
+	 */
+	public static boolean between(int val, int min, int max) {
+		return min <= val && val <= max;
+	}
 
 	public static void printMapInfo(GameInfo info, final PrintStream out)
 			throws AssertionError {

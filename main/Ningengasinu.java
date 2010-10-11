b1a86cbf07154cd605dafdb3695ce730908b5851
todesking <discommunicative@gmail.com>
2010-10-11 16:31:38 +0900

    remove unused files

--- main/Ningengasinu.java
+++
@@ -1,8 +0,0 @@
-import com.todesking.castleatack.NingengasinuAI;
-import com.todesking.castleatack.PlayerAdapter;
-
-public class Ningengasinu extends PlayerAdapter {
-	public Ningengasinu() {
-		super(new NingengasinuAI());
-	}
-}
